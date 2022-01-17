bd81528cdc8dd443fde73f1c57a615c265400788
Ivan <uvanes21@yandex.ru>
2022-01-17 22:26:16 +0300

    Домашнее задание готово для проверки

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //solution1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        //solution2
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + upperFullName);
+
+        //solution3
+        String newFullName = fullName.replace(" ", "; ");
+        System.out.println("Данные ФИО сотрудника для административного отдела - " + newFullName);
+
+        //solution4
+        String fullName2 = "Иванов Семён Семёнович";
+        String newFullName2 = fullName2.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + newFullName2);
     }
 }
